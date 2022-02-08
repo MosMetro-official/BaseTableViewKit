0425311a5a535dc00636b328848382d3c3e65aa3
Viacheslav Platonov <slava.p12@yandex.ru>
2022-02-08 15:13:51 +0300

    update .gitignore

---
+++ Example/Pods/Target Support Files/BaseTableViewKit/BaseTableViewKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BaseTableViewKit : NSObject
+@end
+@implementation PodsDummy_BaseTableViewKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-BaseTableViewKit_Tests/Pods-BaseTableViewKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BaseTableViewKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BaseTableViewKit_Tests
+@end
